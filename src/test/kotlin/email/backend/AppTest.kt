3dcca5751c906cf6d4a006f50a5a206307c7d4fc
tmert1012 <nathan@isidorefarm.com>
2021-05-01 11:17:58 -0400

    first commit

---
+++ src/test/kotlin/email/backend/AppTest.kt
@@ -0,0 +1,13 @@
+package email.backend
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class AppTest {
+    /*
+    @Test fun testAppHasAGreeting() {
+        val classUnderTest = App()
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    }
+    */
+}
